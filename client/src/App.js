568935ae20db98d43f3f1ea93b49f8fe2cbd09a6
Wee Jerrick <weejerrick@gmail.com>
2021-08-12 19:29:02 +0800

    feat: 🎸 Add might mode toggle function

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
+import { useState } from "react";
 import { ThemeProvider, CssBaseline } from "@material-ui/core";
 import { createTheme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-
-const theme = createTheme({
-  palette: {
-    type: "dark",
-  },
-  typography: {
-    fontFamily: "Quicksand",
-    h1: {
-      fontSize: 40,
-      fontWeight: 500,
-    },
-  },
-});
+import Calendar from "./components/Calendar";
+import Container from "@material-ui/core/Container";
+import IconButton from "@material-ui/core/IconButton";
+import WbSunnyIcon from "@material-ui/icons/WbSunny";
+import NightsStayIcon from "@material-ui/icons/NightsStay";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const theme = createTheme({
+    palette: {
+      type: darkMode === true ? "dark" : "light",
+    },
+    typography: {
+      fontFamily: "Quicksand",
+      h1: {
+        fontSize: 40,
+        fontWeight: 500,
+      },
+    },
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Typography variant="h1">App here</Typography>
+      <Container>
+        <Typography variant="h1">Nieves Observation Planner</Typography>
+        <IconButton onClick={() => setDarkMode(!darkMode)}>
+          {darkMode === true ? <WbSunnyIcon /> : <NightsStayIcon />}
+        </IconButton>
+        <Calendar />
+      </Container>
     </ThemeProvider>
   );
 }
---
+++ client/src/components/Calendar.js
@@ -0,0 +1,42 @@
+import "date-fns";
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+
+export default function Calendar() {
+  const [selectedDate, setSelectedDate] = React.useState(
+    new Date().toISOString().slice(0, 10)
+  );
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    console.log(`${date.toISOString().slice(0, 10)} 00:00:00`);
+    // pass this to useEffect
+  };
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      {/* <Grid container justifyContent="space-around"> */}
+      <Grid container>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Select Date"
+          value={selectedDate}
+          onChange={handleDateChange}
+          autoOk={true}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
+}
